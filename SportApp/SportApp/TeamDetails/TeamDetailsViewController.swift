7bae31882474ef1850ae71856d275aa0963bf28a
RahmaAyman <rahmaayman@RahmaAymans-Mac.local>
2021-04-20 12:28:34 -0700

    models and teams details ui

---
+++ SportApp/SportApp/TeamDetails/TeamDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamDetailsViewController.swift
+//  SportApp
+//
+//  Created by RahmaAyman on 4/18/21.
+//  Copyright © 2021 None. All rights reserved.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
